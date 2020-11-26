6da96bac4bc20a6faad4d17550eeccbca9c0114d
fly2sky2000 <fly2sky2000@sina.com.cn>
2020-11-26 22:11:25 +0800

    前端增加代理

--- ui01/src/router/index.js
+++ ui01/src/router/index.js
@@ -5,6 +5,7 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  
   {
     path: '/',
     name: 'Home',
@@ -25,3 +26,22 @@ const router = new VueRouter({
 })
 
 export default router
+
+// export const constantRouterMap = [
+//   //配置默认的路径，默认显示登录页
+//   { path: '/', component: () => import('@/views/login')},
+
+//   //配置登录成功页面，使用时需要使用 path 路径来实现跳转
+//   { path: '/home', component: () => import('@/views/home')},
+//    //配置用户注册页面
+//   { path: '/adduser', component: () => import('@/views/user/adduser')},
+
+//   //配置登录失败页面，使用时需要使用 path 路径来实现跳转
+//   { path: '/error', component: () => import('@/views/error'), hidden: true }
+// ]
+
+// export default new VueRouter({
+//   // mode: 'history', //后端支持可开
+//   scrollBehavior: () => ({ y: 0 }),
+//   routes: constantRouterMap //指定路由列表
+// })
--- ui01/vue.config.js
+++ ui01/vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  publicPath: '/ui01'
+  publicPath: '/ui01',
+  devServer: {
+    open: true,
+    port: '8081',
+    proxy: {
+      '/rest01': {
+        target: 'http://localhost:8188/rest01/',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/rest01': ''
+        }
+      }
+   }
+  }
 }
